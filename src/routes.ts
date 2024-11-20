863c445d4ad93eb941de821718f2f5515d788574
marcelorafael <marcelorafael23@hotmail.com>
2024-11-20 16:48:35 -0300

    feat: add base middleware

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,15 @@ import express, { Router, Request, Response } from 'express'
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserControler';
+
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle as any)
 router.post("/session", new AuthUserController().handle as any)
+
+router.get("/userDetail", isAuthenticated, new DetailUserController().handle as any)
 
 export { router }
