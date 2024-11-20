256d1fbce97cf826974ff267fa6a8fda9ee60184
marcelorafael <marcelorafael23@hotmail.com>
2024-11-20 15:32:45 -0300

    feat: AuthService ok

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import express, { Router, Request, Response } from 'express'
 
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle as any)
+router.post("/session", new AuthUserController().handle as any)
 
 export { router }
