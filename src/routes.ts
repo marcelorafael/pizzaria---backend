9e5094f9e4d4d0afe3493afc894ea27b3f47d534
marcelorafael <marcelorafael23@hotmail.com>
2024-11-20 08:33:53 -0300

    feat: add cors library

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,8 @@ import express, { Router, Request, Response } from 'express'
 const router = Router();
 
 router.get('/test', (req: Request, res: Response): any => {
-    // return res.json({ ok: true })
-    throw new Error('Error jogador')
+    return res.json({ ok: true })
+    // throw new Error('Error jogador')
 });
 
 export { router }
