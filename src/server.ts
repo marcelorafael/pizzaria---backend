92b7ca94acca521140952a97f34137793a489e47
marcelorafael <marcelorafael23@hotmail.com>
2024-11-20 08:29:41 -0300

    feat: add middleware for error

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors";
 
 import { router } from './routes';
 
@@ -8,4 +9,17 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(3333, () => console.log("servidor online!!!"));
+app.use((err: Error, _req: Request, res: Response, next: NextFunction): any => {
+    if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+});
+
+app.listen(3333, () => console.log("servidor online!!! "));
