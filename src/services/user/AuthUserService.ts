2fd8169ffa6e33c9a5c5c8d4e8a49c64004f043c
marcelorafael <marcelorafael23@hotmail.com>
2024-11-20 16:07:13 -0300

    feat: add JWT

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
-import prismaClient from '../../prisma'
+import prismaClient from '../../prisma';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface AuthRequest {
     email: string;
@@ -9,22 +10,39 @@ interface AuthRequest {
 class AuthUserService {
     async execute({ email, password }: AuthRequest) {
         const user = await prismaClient.user.findFirst({
-            where:{
+            where: {
                 email: email
             }
         })
 
-        if(!user){
+        if (!user) {
             throw new Error("User/password incorrect")
         }
 
         const passwordMatch = await compare(password, user.password)
 
-        if(!passwordMatch){
+        if (!passwordMatch) {
             throw new Error("User/password incorrect")
         }
 
-        return { ok: true }
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
