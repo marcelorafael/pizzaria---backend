59bf6f3e179efa3f73e2c9433d5398347437611f
marcelorafael <marcelorafael23@hotmail.com>
2024-11-20 15:05:48 -0300

    feat: add returns data users

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma'
+
 interface UserRequest {
     name: string;
     email: string;
@@ -6,7 +8,34 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
-        return { name: name }
+        if(!email){
+            throw new Error("Email incorrect")
+        }
+
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("User already exists")
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+        return user;
     }
 }
 
