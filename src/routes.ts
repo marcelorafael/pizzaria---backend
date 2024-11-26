909c91d9b55e4ae7a14f3574dc6968be86795a0a
marcelorafael <marcelorafael23@hotmail.com>
2024-11-26 20:56:45 -0300

    feat: category service ok

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,19 @@ import express, { Router, Request, Response } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserControler';
+import { CreateCategoryController } from './controllers/cetegory/CreateCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
+// ROUTES USER
 router.post("/users", new CreateUserController().handle as any)
 router.post("/session", new AuthUserController().handle as any)
 
 router.get("/userDetail", isAuthenticated as any, new DetailUserController().handle as any)
+
+// ROUTES CATEGORY
+router.post("/category", isAuthenticated as any, new CreateCategoryController().handle as any)
 
 export { router }
